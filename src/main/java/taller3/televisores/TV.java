3ffa83365fda2c7307dc0c20d5e4dae4f1403fcb
Wakamole2002 <luvarelao@unal.edu.co>
2023-02-25 20:50:58 -0500

    commit 4

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -8,7 +8,7 @@ public class TV {
     private boolean estado;
     private int volumen;
     private Control control;
-    private static int numTV;
+    public static int numTV;
     
     
     //Sertters
